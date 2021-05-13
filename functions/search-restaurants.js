ff392c6542313de8301e72f50bf8d5010ae5d148
yyavorskyi <yyavorskyi@pipingrock.com>
2021-05-13 11:47:22 +0300

    week 3 begining

--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,7 +1,9 @@
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
 const dynamodb = new DocumentClient()
 
-const defaultResults = process.env.defaultResults || 8
+const { serviceName, stage } = process.env
 const tableName = process.env.restaurants_table
 
 const findRestaurantsByTheme = async (theme, count) => {
@@ -18,14 +20,24 @@ const findRestaurantsByTheme = async (theme, count) => {
   return resp.Items
 }
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = middy(async (event, context) => {
   const req = JSON.parse(event.body)
   const theme = req.theme
-  const restaurants = await findRestaurantsByTheme(theme, defaultResults)
+  const restaurants = await findRestaurantsByTheme(theme, context.config.defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
   }
-
+  console.info(context.secretString)
   return response
-}
+}).use(
+  ssm({
+    cache: true,
+    cacheExpiryInMillis: 1 * 60 * 1000, // 1 mins
+    setToContext: true,
+    fetchData: {
+      config: `/${serviceName}/${stage}/search-restaurants/config`,
+      secretString: `/${serviceName}/${stage}/search-restaurants/secretString`
+    }
+  })
+)
